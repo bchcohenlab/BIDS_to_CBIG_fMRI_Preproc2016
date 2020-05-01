a344260abc95bf291afae6442077b7d3a3887749
Alexander Cohen <alexcohen@gmail.com>
2020-05-01 06:29:40 -0400

    initial files

---
+++ BIDS_CBIG_fMRI_preprocess.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# Script to run the CBIG Preproc2016 pipeline for a single subject in BIDS format
+
+
+if [ $# -lt 1 ] ; then
+	echo "Usage: $0 <bids_dir> <participant_id> <config_file>"
+	echo "e.g. $0 /fileserver/corrin/connectome/GSP sub-001 /fileserver/corrin/connectome/GSP/code/FS_to_MNI.config"
+	echo "====================================="
+	echo ""
+	echo "This script..."
+	echo "output:"
+	echo "In <bids_dir>/derivatives/CBIG_fMRI_preprocess/<participant_id>:"
+	echo "1) "
+	echo "2) "
+	echo "3) "
+	echo ""
+	echo "====================================="
+	echo ""
+	echo ""
+	exit 1 ;
+fi
+
+bids_dir=$1
+participant_id=$2
+config_file=$3
+config_name=`basename ${config_file} .config`
+
+# Run CBIG initialization script:
+BIDSPATH=${bids_dir}/code # path to scripts
+source ${BIDSPATH}/CBIG_caladan_config.sh
+
+
+# Create output directory:
+output_dir=${bids_dir}/derivatives/CBIG_fMRI_preprocess_${config_name}
+mkdir -p ${output_dir}/${participant_id}
+
+# Make copy of config file:
+cp ${config_file} ${output_dir}
+
+# Create fmrinii file from BIDS-formatted files:
+fmrinii_file=${output_dir}/${participant_id}/${participant_id}_bold.fmrinii
+ls -1 ${bids_dir}/${participant_id}/func/${participant_id}_*_bold.nii.gz | nl -w2 -n rz \
+	> ${fmrinii_file}
+
+# Create slice timing file from BIDS-formatted json files:
+export st_file=${output_dir}/${participant_id}/${participant_id}_bold.slicetiming
+if [ -f "${bids_dir}/task-rest_bold.json" ]; then
+	jq .'SliceTiming'[] ${bids_dir}/task-rest_bold.json > $st_file
+	echo "Used ${bids_dir}/task-rest_bold.json for slice timing information"
+
+elif [ -f "${bids_dir}/${participant_id}/func/task-rest_bold.json" ]; then
+	jq .'SliceTiming'[] ${bids_dir}/${participant_id}/func/task-rest_bold.json > $st_file
+	echo "Used ${bids_dir}/${participant_id}/func/task-rest_bold.json for slice timing information"
+
+else
+	echo "Could not find a task-rest_bold json file for slice timing information"
+	exit 0
+fi
+
+# Check for freesurfer output:
+freesurfer_dir=${bids_dir}/derivatives/freesurfer
+if [ -f "${freesurfer_dir}/${participant_id}/mri/brain.mgz" ]; then
+	echo "Found freesurfer output"
+
+else
+	echo "Could not find freesurfer output"
+	exit 0
+fi
+
+
+CBIG_preproc_fMRI_preprocess.csh \
+	-s  ${participant_id} \
+	-fmrinii  ${fmrinii_file} \
+	-anat_s  ${participant_id} \
+	-anat_d  ${freesurfer_dir} \
+	-output_d  ${output_dir} \
+	-config  ${config_file} \
+
---
+++ CBIG_caladan_config.sh
@@ -0,0 +1,45 @@
+#! /bin/sh
+#
+
+# DO NOT CHANGE: This clears old freesurfer variables if they previously exists
+if [ -n "$FREESURFER_HOME" ]; then
+	$FREESURFER_HOME/bin/clear_fs_env.csh 
+fi
+
+export caladan_opt=/fileserver/caladan_ssd/opt
+
+# PLEASE CHANGE: Please specify location of CBIG repository
+export CBIG_CODE_DIR=/fileserver/caladan_ssd/repos/CBIG
+
+# PLEASE CHANGE: define locations for these libraries
+export FREESURFER_HOME=${caladan_opt}/freesurfer
+export CBIG_MATLAB_DIR=/opt/matlab/current
+export CBIG_SPM_DIR=${caladan_opt}/spm12
+export CBIG_AFNI_DIR=/home/ch186161/abin
+export CBIG_ANTS_DIR=${caladan_opt}/ANTs/install/bin
+export CBIG_WB_DIR=${caladan_opt}/workbench
+export CBIG_FSLDIR=${caladan_opt}/fsl
+
+# DO NOT CHANGE: define locations for unit tests data and replication data
+export CBIG_TESTDATA_DIR=/mnt/eql/yeo1/CBIG_test_data/unit_tests
+export CBIG_REPDATA_DIR=/mnt/eql/yeo1/CBIG_test_data/replication
+
+# DO NOT CHANGE: define scheduler location
+export CBIG_SCHEDULER_DIR=/apps/sysapps/TORQUE/bin
+
+# DO NOT CHANGE: set up your environment with the configurations above
+SETUP_PATH=$CBIG_CODE_DIR/setup/CBIG_generic_setup.sh
+source $SETUP_PATH
+
+# DO NOT CHANGE: set up temporary directory for MRIread from FS6.0 for CBIG 
+# members using the HPC, Other users should comment this out
+#export TMPDIR=/tmpstore
+
+# Do NOT CHANGE: set up MATLABPATH so that MATLAB can find startup.m in our repo 
+export MATLABPATH=$CBIG_CODE_DIR/setup
+
+# specified the default Python environment.
+# Please UNCOMMENT if you follow CBIG's set up for Python environments.
+# We use Python version 3.5 as default.
+# Please see $CBIG_CODE_DIR/setup/python_env_setup/README.md for more details.
+# source activate CBIG_py3
