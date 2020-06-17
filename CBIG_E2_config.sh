8118a89f9eb1bcae7e2a5df51a866820916545fb
Alexander Li Cohen <alexcohen@gmail.com>
2020-06-17 12:02:17 -0400

    adding E2 config

---
+++ CBIG_E2_config.sh
@@ -0,0 +1,58 @@
+#! /bin/sh
+#
+
+# DO NOT CHANGE: This clears old freesurfer variables if they previously exists
+if [ -n "$FREESURFER_HOME" ]; then
+	$FREESURFER_HOME/bin/clear_fs_env.csh 
+fi
+
+### activating software on E2:
+# Load biogrids (provides: afni, ANTs, datalad, freesurfer v6.0.1, spm, and wb_command)
+source /programs/biogrids.shrc
+export BIOGRIDS_BIN=/programs/x86_64-linux/system/biogrids_bin
+
+#Load FSL
+module load fsl
+source ${FSLDIR}/etc/fslconf/fsl.sh
+
+# Load Specific Freesurfer version
+export FS_LICENSE=/programs/local/licenses/freesurfer/license.txt
+export SUBJECTS_DIR=/lab-share/Neuro-Cohen-e2/Public/projects/subjects
+export FREESURFER_HOME=/lab-share/Neuro-Cohen-e2/Public/software/freesurfer_v5.3.0
+source ${FREESURFER_HOME}/SetUpFreeSurfer.sh
+
+# PLEASE CHANGE: Please specify location of CBIG repository
+export CBIG_CODE_DIR=/lab-share/Neuro-Cohen-e2/Public/repos/Standalone_CBIG_fMRI_Preproc2016
+
+# PLEASE CHANGE: define locations for these libraries
+export FREESURFER_HOME=${caladan_opt}
+export CBIG_MATLAB_DIR=/opt/matlab/current
+export CBIG_SPM_DIR=${caladan_opt}/spm12
+export CBIG_AFNI_DIR=/home/ch186161/abin
+export CBIG_ANTS_DIR=${caladan_opt}/ANTs/install/bin
+export CBIG_WB_DIR=/programs/x86_64-linux/workbench
+export CBIG_FSLDIR=${FSLDIR}
+
+# DO NOT CHANGE: define locations for unit tests data and replication data
+export CBIG_TESTDATA_DIR=/mnt/eql/yeo1/CBIG_test_data/unit_tests
+export CBIG_REPDATA_DIR=/mnt/eql/yeo1/CBIG_test_data/replication
+
+# DO NOT CHANGE: define scheduler location
+export CBIG_SCHEDULER_DIR=/apps/sysapps/TORQUE/bin
+
+# DO NOT CHANGE: set up your environment with the configurations above
+SETUP_PATH=$CBIG_CODE_DIR/setup/CBIG_generic_setup.sh
+source $SETUP_PATH
+
+# DO NOT CHANGE: set up temporary directory for MRIread from FS6.0 for CBIG 
+# members using the HPC, Other users should comment this out
+#export TMPDIR=/tmpstore
+
+# Do NOT CHANGE: set up MATLABPATH so that MATLAB can find startup.m in our repo 
+export MATLABPATH=$CBIG_CODE_DIR/setup
+
+# specified the default Python environment.
+# Please UNCOMMENT if you follow CBIG's set up for Python environments.
+# We use Python version 3.5 as default.
+# Please see $CBIG_CODE_DIR/setup/python_env_setup/README.md for more details.
+# source activate CBIG_py3
